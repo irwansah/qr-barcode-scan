94a799d5248d291146026d7b2f694763a5853ed8
irwansah <ats.irwans@xl.co.id>
2023-09-07 12:26:11 +0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,31 +3,17 @@ import { QrReader } from "react-qr-reader_multiformatreader";
 
 const Home = () => {
   const [data, setData] = useState("No result");
-  const [facingMode, setFacing] = useState<string>("");
-
-  const handleSwitchCamera = () => {
-    localStorage.setItem(
-      "facingMode",
-      localStorage.getItem("facingMode") == `user` ? `environment` : `user`
-    );
-    location.reload();
-  };
-
-  useEffect(() => {
-    if (localStorage.getItem("facingMode") == null) {
-      localStorage.setItem("facingMode", "environment");
-    } else {
-      setFacing(localStorage.getItem("facingMode") ?? "environment");
-    }
-  }, []);
 
   return (
     <>
       <div className="flex justify-center min-h-screen items-center place-items-center ">
         <div className="w-full md:w-1/2 m-auto p-2">
+        <p className="relative bg-black rounded-lg p-3 text-center border w-full mb-3">
+          {data}
+        </p>
           <QrReader
             constraints={{
-              facingMode: facingMode,
+              facingMode: "environment",
               width: { ideal: 1280 },
               height: { ideal: 720 },
             }}
@@ -44,16 +30,8 @@ const Home = () => {
             videoContainerStyle={{ width: "100%" }}
             className="rounded-lg overflow-hidden"
           />
-          
         </div>
-        <p className="absolute bg-black rounded-lg p-3 md:top-5 top-[20%]">
-            {data}
-          </p>
-          <p className="absolute bg-black rounded-lg p-3 md:bottom-5 bottom-[20%]">
-            <button onClick={() => handleSwitchCamera()}>
-              Switch {facingMode}
-            </button>
-          </p>
+        
       </div>
     </>
   );
