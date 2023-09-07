c48dcecbf1d220b1b103de6a5b96633725a73c72
irwansah <ats.irwans@xl.co.id>
2023-09-07 11:44:35 +0700

    test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,52 @@
-/** @type {import('next').NextConfig} */
+const ContentSecurityPolicy = `frame-ancestors 'none'`;
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    swcMinify: true,
+   
+    async headers() {
+        return [
+            {
+                source: "/:path*",
+                headers: [
+                    {
+                        key: "X-Content-Type-Options",
+                        value: "nosniff",
+                    },
+                    {
+                        key: "X-Frame-Options",
+                        value: "DENY",
+                    },
+                    {
+                        key: "X-XSS-Protection",
+                        value: "1; mode=block",
+                    },
+                    {
+                        key: "X-DNS-Prefetch-Control",
+                        value: "on",
+                    },
+                    {
+                        key: "Referrer-Policy",
+                        value: "origin-when-cross-origin",
+                    },
+                    {
+                        key: "Permissions-Policy",
+                        value: "camera=(self), microphone=(), geolocation=(self), interest-cohort=()",
+                    },
+                    {
+                        key: "Strict-Transport-Security",
+                        value: "max-age=63072000; includeSubDomains; preload",
+                    },
+                    {
+                        key: "Content-Security-Policy",
+                        value: ContentSecurityPolicy.replace(
+                            /\s{2,}/g,
+                            " "
+                        ).trim(),
+                    },
+                ],
+            },
+        ];
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
